5878efcfbfd0a93201e80c59eb97f87317ff756e
rabucejo <rogerhapay@gmail.com>
2023-04-15 20:28:17 +0800

    centered upload btn

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -16,25 +16,30 @@
                         </div>
                     @endif
                     {{-- FILE UPLOAD FORM --}}
-                    <form method="POST" action="{{route('store')}}">
+                    <form method="POST" action="{{ route('store') }}">
                         @csrf
                         {{-- FILENAME --}}
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="Filename" name="Filename"  >
-                            <label for="floatingInput" >Filename</label>
+                            <input type="text" class="form-control" id="floatingInput" placeholder="Filename"
+                                name="Filename">
+                            <label for="floatingInput">Filename</label>
                         </div>
                         {{-- PROGRAM TYPE --}}
-                        <select class="form-select mb-3" name="FilePath" aria-label="Default select example" aria-placeholder="Folder Location">
+                        <select class="form-select mb-3" name="FilePath" aria-label="Default select example"
+                            aria-placeholder="Folder Location">
                             <option value="SETUP">SETUP</option>
                             <option value="GIA">GIA</option>
                             <option value="OTHERS">OTHERS</option>
                         </select>
                         {{-- DESCRIPTION --}}
                         <div class="form-floating mb-3">
-                           <input type="text" class="form-control" id="floatingInput" placeholder="Description" name="Description"  >
+                            <input type="text" class="form-control" id="floatingInput" placeholder="Description"
+                                name="Description">
                             <label for="floatingInput">Description</label>
                         </div>
-                        <button type="submit" class="btn btn-primary ">Upload</button>
+                        <div class="text-center">
+                             <button type="submit" class="btn btn-primary ">Upload</button>
+                        </div>
                     </form>
                 </div>
             </div>
