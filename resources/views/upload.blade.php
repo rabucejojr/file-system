587e91799e6bf1ec51fcf3d915ed4bd8dac7ef22
rabucejo <rogerhapay@gmail.com>
2023-04-15 20:36:58 +0800

    rearrange and rename components

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -18,27 +18,32 @@
                     {{-- FILE UPLOAD FORM --}}
                     <form method="POST" action="{{ route('store') }}">
                         @csrf
-                        {{-- FILENAME --}}
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="Filename"
-                                name="Filename">
-                            <label for="floatingInput">Filename</label>
-                        </div>
                         {{-- PROGRAM TYPE --}}
-                        <select class="form-select mb-3" name="FilePath" aria-label="Default select example"
+                        <select class="form-select mb-3" name="FilePath" aria-label="Default select example" name="FileFolder">
                             aria-placeholder="Folder Location">
                             <option value="SETUP">SETUP</option>
                             <option value="GIA">GIA</option>
                             <option value="OTHERS">OTHERS</option>
                         </select>
+                        {{-- FILENAME --}}
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="Filename" name="Filename">
+                            <label for="floatingInput">Filename</label>
+                        </div>
                         {{-- DESCRIPTION --}}
                         <div class="form-floating mb-3">
                             <input type="text" class="form-control" id="floatingInput" placeholder="Description"
-                                name="Description">
+                                name="FileDescription">
                             <label for="floatingInput">Description</label>
                         </div>
+                        {{-- LOCATION --}}
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="Location"
+                                name="FilePath">
+                            <label for="floatingInput">Location</label>
+                        </div>
                         <div class="text-center">
-                             <button type="submit" class="btn btn-primary ">Upload</button>
+                            <button type="submit" class="btn btn-primary ">Upload</button>
                         </div>
                     </form>
                 </div>
