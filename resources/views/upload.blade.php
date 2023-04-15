dc99d05d6b228c60158d9a219c02e6bcf8f9b4c3
rabucejo <rogerhapay@gmail.com>
2023-04-15 20:57:33 +0800

    space; trial only

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -36,6 +36,7 @@
                                 name="FileDescription">
                             <label for="floatingInput">Description</label>
                         </div>
+                        
                         {{-- LOCATION --}}
                         <div class="form-floating mb-3">
                             <input type="text" class="form-control" id="floatingInput" placeholder="Location"
