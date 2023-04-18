b04d61828cc7f405bb270617688018c96a22c1d1
rabucejo <rogerhapay@gmail.com>
2023-04-18 15:23:55 +0800

    refactored

--- resources/views/file_table.blade.php
+++ resources/views/file_table.blade.php
@@ -5,10 +5,9 @@
             <div class="bg-secondary rounded h-100 p-4">
                 <h6 class="mb-4">File Summary</h6>
                 {{-- <div class="px-0 w-25 my-2"> --}}
-                    <form class="d-none d-md-flex ms-4" action="{{route('search')}}" method="POST">
+                <form class="d-none d-md-flex ms-4" action="{{ route('search') }}" method="POST">
                     @csrf
-                    <input class="form-control bg-dark border-0" type="search" name="Search"
-                        placeholder="Search File">
+                    <input class="form-control bg-dark border-0" type="search" name="Search" placeholder="Search File">
                     <button class="btn btn-primary border-0 mx-2">Search</button>
                 </form>
                 {{-- </div> --}}
