26f182dbd1037dd8fe9922fb5ad72b69ab6c555a
rabucejo <rogerhapay@gmail.com>
2023-04-20 16:19:57 +0800

    initial setup for edit modal

--- resources/views/file_table.blade.php
+++ resources/views/file_table.blade.php
@@ -52,13 +52,9 @@
     <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content" style="background-color:#191C24 !important">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
                 <div class="modal-body">
                     <div class="bg-secondary rounded h-200 p-4">
-                        <h6 class="mb-4">File Upload Form</h6>
+                        <h6 class="mb-4">File Update</h6>
                         @if (isset($Success))
                             <div class="alert alert-success" role="alert">
                                 {{ $Success }}
@@ -101,10 +97,6 @@
                             </div>
                         </form>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
