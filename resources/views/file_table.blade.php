d00de320c993f65c885f2698604cff72b20d8c3b
rabucejo <rogerhapay@gmail.com>
2023-04-26 15:15:32 +0800

    partial edit on modal update

--- resources/views/file_table.blade.php
+++ resources/views/file_table.blade.php
@@ -17,9 +17,9 @@
                         <thead>
                             <tr>
                                 <th scope="col">No.</th>
-                                <th scope="col">Filename</th>
                                 <th scope="col">Folder</th>
-                                <th scope="col">Location</th>
+                                <th scope="col">Filename</th>
+                                <th scope="col">Path</th>
                                 <th scope="col">Description</th>
                                 <th scope="col">Operation</th>
                             </tr>
@@ -30,12 +30,12 @@
                                 @foreach ($files as $file)
                                     <tr>
                                         <td>{{ $file->FileId }}</td>
-                                        <td>{{ $file->Filename }}</td>
                                         <td>{{ $file->FileFolder }}</td>
+                                        <td>{{ $file->Filename }}</td>
                                         <td>{{ $file->FilePath }}</td>
                                         <td>{{ $file->FileDescription }}</td>
                                         <td>
-                                            <button type="button" onclick="edit('{{$file->Filename}}')" class="btn btn-info">Edit</button>
+                                            <button type="button" onclick="edit('{{$file->FileFolder}}','{{$file->Filename}}','{{$file->FileDescription}}','{{$file->FilePath}}')" class="btn btn-info">Edit</button>
                                             <button type="submit" class=" btn btn-primary">Delete</button>
                                         </td>
                                     </tr>
@@ -67,7 +67,7 @@
                         <form method="POST" action="{{ route('store') }}">
                             @csrf
                             {{-- PROGRAM TYPE --}}
-                            <select class="form-select mb-3" aria-label="Default select example" name="FileFolder">
+                            <select class="form-select mb-3" id="filefolder" aria-label="Default select example" name="FileFolder">
                                 aria-placeholder="Folder Location">
                                 <option value="SETUP">SETUP</option>
                                 <option value="GIA">GIA</option>
@@ -87,12 +87,12 @@
                             </div>
                             {{-- LOCATION --}}
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control fileloc" id="floatingInput" placeholder="Location"
-                                    name="FilePath">
-                                <label for="floatingInput">Location</label>
+                                <input type="text" class="form-control filepath" id="floatingInput" placeholder="Path"
+                                    name="FilePath" disabled readonly>
+                                <label for="floatingInput">Path</label>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-primary ">Upload</button>
+                                <button type="submit" onclick="save_edit()" class="btn btn-primary ">SAVE</button>
                             </div>
                         </form>
                     </div>
@@ -112,11 +112,51 @@
             });
         });
         // table buttons
-        function edit(filename){
+        function edit(FileFolder,Filename,FileDescription,FilePath){
             // get inputs
             $('#edit_file').modal('toggle');
-            var filename = $('.filename').val();
+            $('#filefolder').val(FileFolder);
+            $('.filename').val(Filename);
+            $('.filedesc').val(FileDescription);
+            $('.filepath').val(FilePath);
         
+        }
+        function save_edit(){
+            var FileFolder = $('#filefolder').val(FileFolder);
+            var Filename = $('.filename').val(Filename);
+            var FileDescription = $('.filedesc').val(FileDescription);
+            var FilePath = $('.filepath').val(FilePath);
+
+            var file_data = new FormData();
+            file_data.append('FileFolder',FileFolder)
+            file_data.append('Filename',Filename)
+            file_data.append('FileDescription',FileDescription)
+            file_data.append('FilePath',FilePath)
+
+            $.ajax({
+            method: "POST",
+            url: "{{route('store')}}",
+            dataType: 'json',
+            processData:false,
+            contentType:false,
+            cache:false,
+            async:false,
+            data: student_Data,
+        }).done(function( msg ) {
+            if(msg.result ==true){
+                Swal.fire(
+                'Update',
+                msg.message,
+                'success'
+                )
+            } else {
+                Swal.fire(
+                'Update',
+                msg.message,
+                'error'
+                )
+            }
+        });
         }
     </script>
 @endsection
